5d4ce3c41fb7bcfa354cf5af9cb683f014a0cbb0
lgp19 <lgp19@AERO-DO-LG>
2023-06-16 20:48:32 -0300

    Add project files.

---
+++ HackerRankLgp/PlusMinus/Exercise.cs
@@ -0,0 +1,32 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+        double total = arr.Count();
+        Console.WriteLine(string.Join(Environment.NewLine, new[] {
+            arr.Count(s => s < 0) / total,
+            arr.Count(s => s > 0) / total,
+            arr.Count(s => s == 0) / total,
+        }));
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        var n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        var arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(arr);
+    }
+}
