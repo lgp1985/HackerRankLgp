76d59be98d7afd8f6285da34ea111309f65094f7
Luis Gustavo Pereira <lgp1985@outlook.com>
2023-06-16 20:53:06 -0300

    Create PlusMinus

---
+++ PlusMinus/Class1.cs
@@ -0,0 +1,27 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        var n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        var arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(arr);
+    }
+}
