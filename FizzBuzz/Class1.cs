7c0a23a85974e7a9d5589e20537363f8c89a1bb2
Luis Gustavo Pereira <lgp1985@outlook.com>
2023-06-16 21:35:08 -0300

    solution to FizzBuzz

---
+++ FizzBuzz/Class1.cs
@@ -0,0 +1,29 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'fizzBuzz' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void fizzBuzz(int n)
+    {
+        var response = string.Empty;
+        if (n % 3 == 0) { response += "Fizz"; }
+        if (n % 5 == 0) { response += "Buzz"; }
+        if (response.Length == 0) { response = n.ToString(); }
+        Console.WriteLine(response);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        var n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.fizzBuzz(n);
+    }
+}
