a77397330581f02241bf8cc1bd1fa9240b66c59f
Luis Gustavo Pereira <lgp1985@outlook.com>
2023-06-17 00:07:16 -0300

    Solution to DiagonalDiference

--- DiagonalDifference/Program.cs
+++ DiagonalDifference/Program.cs
@@ -1,18 +1,4 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
-
-class Result
+﻿class Result
 {
 
     /*
@@ -24,7 +10,18 @@ class Result
 
     public static int diagonalDifference(List<List<int>> arr)
     {
-
+        int principalDiagonal = 0, secondaryDiagonal = 0;
+        for (var i = 0; i < arr.Count; i++)
+        {
+            for (var j = 0; j < arr[i].Count; j++)
+            {
+                if (i == j)
+                { principalDiagonal += arr[i][j]; }
+                if ((i + j) == (arr.Count - 1))
+                { secondaryDiagonal += arr[i][j]; }
+            }
+        }
+        return Math.Abs(principalDiagonal - secondaryDiagonal);
     }
 
 }
@@ -35,16 +32,16 @@ class Solution
     {
         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-        int n = Convert.ToInt32(Console.ReadLine().Trim());
+        var n = Convert.ToInt32(Console.ReadLine().Trim());
 
-        List<List<int>> arr = new List<List<int>>();
+        var arr = new List<List<int>>();
 
-        for (int i = 0; i < n; i++)
+        for (var i = 0; i < n; i++)
         {
             arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
         }
 
-        int result = Result.diagonalDifference(arr);
+        var result = Result.diagonalDifference(arr);
 
         textWriter.WriteLine(result);
 
