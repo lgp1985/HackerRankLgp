aa1313eb5160562581726ac08a2d836a4bd2e71c
Luis Gustavo Pereira <lgp1985@outlook.com>
2023-06-18 17:48:55 -0300

    Solution of PalindromeIndex

--- PalindromeIndex/Program.cs
+++ PalindromeIndex/Program.cs
@@ -1,20 +1,4 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
-
-
-
-class Result
+﻿class Result
 {
 
     /*
@@ -26,7 +10,26 @@ class Result
 
     public static int palindromeIndex(string s)
     {
-
+        var start = 0;
+        var end = s.Length - 1;
+        while (start < end && s.ElementAt(start) == s.ElementAt(end))
+        {
+            start++;
+            end--;
+        }
+        if (start >= end) return -1; // already a palindrome
+        if (isPalindrome(s, start + 1, end)) return start;
+        if (isPalindrome(s, start, end - 1)) return end;
+        return -1;
+    }
+    public static bool isPalindrome(string s, int start, int end)
+    {
+        while (start < end && s.ElementAt(start) == s.ElementAt(end))
+        {
+            start++;
+            end--;
+        }
+        return start >= end;
     }
 
 }
@@ -37,13 +40,13 @@ class Solution
     {
         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-        int q = Convert.ToInt32(Console.ReadLine().Trim());
+        var q = Convert.ToInt32(Console.ReadLine().Trim());
 
-        for (int qItr = 0; qItr < q; qItr++)
+        for (var qItr = 0; qItr < q; qItr++)
         {
-            string s = Console.ReadLine();
+            var s = Console.ReadLine();
 
-            int result = Result.palindromeIndex(s);
+            var result = Result.palindromeIndex(s);
 
             textWriter.WriteLine(result);
         }
