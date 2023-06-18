cb137ae4590069499b87b23d06cadd4f6e396bba
Luis Gustavo Pereira <lgp1985@outlook.com>
2023-06-18 19:43:17 -0300

    solution of MergeTwoSortedLinkedLists

--- MergeTwoSortedLinkedLists/Program.cs
+++ MergeTwoSortedLinkedLists/Program.cs
@@ -1,44 +1,39 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+﻿class Solution
+{
 
-class Solution {
-
-    class SinglyLinkedListNode {
+    class SinglyLinkedListNode
+    {
         public int data;
         public SinglyLinkedListNode next;
 
-        public SinglyLinkedListNode(int nodeData) {
+        public SinglyLinkedListNode(int nodeData)
+        {
             this.data = nodeData;
             this.next = null;
         }
     }
 
-    class SinglyLinkedList {
+    class SinglyLinkedList
+    {
         public SinglyLinkedListNode head;
         public SinglyLinkedListNode tail;
 
-        public SinglyLinkedList() {
+        public SinglyLinkedList()
+        {
             this.head = null;
             this.tail = null;
         }
 
-        public void InsertNode(int nodeData) {
-            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+        public void InsertNode(int nodeData)
+        {
+            var node = new SinglyLinkedListNode(nodeData);
 
-            if (this.head == null) {
+            if (this.head == null)
+            {
                 this.head = node;
-            } else {
+            }
+            else
+            {
                 this.tail.next = node;
             }
 
@@ -46,13 +41,16 @@ class Solution {
         }
     }
 
-    static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep, TextWriter textWriter) {
-        while (node != null) {
+    static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep, TextWriter textWriter)
+    {
+        while (node != null)
+        {
             textWriter.Write(node.data);
 
             node = node.next;
 
-            if (node != null) {
+            if (node != null)
+            {
                 textWriter.Write(sep);
             }
         }
@@ -69,36 +67,56 @@ class Solution {
      * }
      *
      */
-    static SinglyLinkedListNode mergeLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+    static SinglyLinkedListNode mergeLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2)
+    {
+        var list = new List<int>();
+        foreach (var item1 in new[] { head1, head2 })
+        {
+            var item = item1;
+            do
+            {
+                list.Add(item.data);
+                item = item.next;
+            } while (item is not null);
+        }
 
-
+        SinglyLinkedListNode output = null;
+        foreach (var item in list.OrderByDescending(s => s))
+        {
+            output = new SinglyLinkedListNode(item) { next = output };
+        }
+        return output;
     }
 
-    static void Main(string[] args) {
+    static void Main(string[] args)
+    {
         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-        int tests = Convert.ToInt32(Console.ReadLine());
+        var tests = Convert.ToInt32(Console.ReadLine());
 
-        for (int testsItr = 0; testsItr < tests; testsItr++) {
-            SinglyLinkedList llist1 = new SinglyLinkedList();
+        for (var testsItr = 0; testsItr < tests; testsItr++)
+        {
+            var llist1 = new SinglyLinkedList();
 
-            int llist1Count = Convert.ToInt32(Console.ReadLine());
+            var llist1Count = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i < llist1Count; i++) {
-                int llist1Item = Convert.ToInt32(Console.ReadLine());
+            for (var i = 0; i < llist1Count; i++)
+            {
+                var llist1Item = Convert.ToInt32(Console.ReadLine());
                 llist1.InsertNode(llist1Item);
             }
-          
-          	SinglyLinkedList llist2 = new SinglyLinkedList();
 
-            int llist2Count = Convert.ToInt32(Console.ReadLine());
+            var llist2 = new SinglyLinkedList();
 
-            for (int i = 0; i < llist2Count; i++) {
-                int llist2Item = Convert.ToInt32(Console.ReadLine());
+            var llist2Count = Convert.ToInt32(Console.ReadLine());
+
+            for (var i = 0; i < llist2Count; i++)
+            {
+                var llist2Item = Convert.ToInt32(Console.ReadLine());
                 llist2.InsertNode(llist2Item);
             }
 
-            SinglyLinkedListNode llist3 = mergeLists(llist1.head, llist2.head);
+            var llist3 = mergeLists(llist1.head, llist2.head);
 
             PrintSinglyLinkedList(llist3, " ", textWriter);
             textWriter.WriteLine();
