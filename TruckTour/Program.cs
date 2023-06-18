a593ece597b6befbebf1170423212766e706ccc2
Luis Gustavo Pereira <lgp1985@outlook.com>
2023-06-18 19:14:47 -0300

    Solution of TruckTour

--- TruckTour/Program.cs
+++ TruckTour/Program.cs
@@ -1,20 +1,4 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
-
-
-
-class Result
+﻿class Result
 {
 
     /*
@@ -26,7 +10,27 @@ class Result
 
     public static int truckTour(List<List<int>> petrolpumps)
     {
-
+        var n = petrolpumps.Count;
+        for (var i = 0; i < n; i++)
+        {
+            var petrol = 0;
+            var index = i;
+            if (petrolpumps[i][0] < petrolpumps[i][1])
+                continue;
+            while (true)
+            {
+                petrol += petrolpumps[index][0];
+                petrol -= petrolpumps[index][1];
+                if (petrol < 0)
+                    break;
+                index = (index + 1) % n;
+                if (index == i)
+                    break;
+            }
+            if (index == i)
+                return i;
+        }
+        return -1;
     }
 
 }
@@ -37,16 +41,16 @@ class Solution
     {
         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-        int n = Convert.ToInt32(Console.ReadLine().Trim());
+        var n = Convert.ToInt32(Console.ReadLine().Trim());
 
-        List<List<int>> petrolpumps = new List<List<int>>();
+        var petrolpumps = new List<List<int>>();
 
-        for (int i = 0; i < n; i++)
+        for (var i = 0; i < n; i++)
         {
             petrolpumps.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(petrolpumpsTemp => Convert.ToInt32(petrolpumpsTemp)).ToList());
         }
 
-        int result = Result.truckTour(petrolpumps);
+        var result = Result.truckTour(petrolpumps);
 
         textWriter.WriteLine(result);
 
