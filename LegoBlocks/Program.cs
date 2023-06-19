29d59e4e349552a8e2134c1471230149bba35462
Luis Gustavo Pereira <lgp1985@outlook.com>
2023-06-19 16:13:19 -0300

    I have no idea of this solution, but here it is

--- LegoBlocks/Program.cs
+++ LegoBlocks/Program.cs
@@ -1,18 +1,4 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
-
-class Result
+﻿class Result
 {
 
     /*
@@ -26,6 +12,51 @@ class Result
 
     public static int legoBlocks(int n, int m)
     {
+        // Write your code here
+        var divisor = 7 + (int)Math.Pow(10, 9);
+        var p = new long[m]; // combination count of one row of m length
+        for (var i = 0; i < m; i++)
+        {
+            if (i == 0) p[i] = 1;
+            else if (i == 1) p[i] = 2;
+            else if (i == 2) p[i] = 4;
+            else if (i == 3) p[i] = 8;
+            else p[i] = (p[i - 1] + p[i - 2] + p[i - 3] + p[i - 4]) % divisor;
+        }
+
+        // all possible (good or bad) combinations count of a n * x (1<=x<=m) wall
+        var a = new long[m];
+        for (var i = 0; i < m; i++)
+        {
+            long base1 = p[i], times = n, res = base1;
+            while (times > 1)
+            {
+                res = (res * base1) % divisor;
+                times--;
+            }
+            a[i] = res;
+        }
+
+        // good combinations count of a n * x (1<=x<=m) wall
+        var g = new long[m];
+        for (var i = 0; i < m; i++)
+        {
+            if (i == 0) g[i] = 1;
+            else
+            {
+                var pGood = a[i];
+                for (var j = 0; j < i; j++)
+                {
+                    pGood -= g[j] * a[i - j - 1] % divisor;
+                }
+                while (pGood < 0)
+                {
+                    pGood += divisor;
+                }
+                g[i] = pGood;
+            }
+        }
+        return (int)(g[m - 1] % divisor);
 
     }
 
@@ -37,17 +68,17 @@ class Solution
     {
         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-        int t = Convert.ToInt32(Console.ReadLine().Trim());
+        var t = Convert.ToInt32(Console.ReadLine().Trim());
 
-        for (int tItr = 0; tItr < t; tItr++)
+        for (var tItr = 0; tItr < t; tItr++)
         {
-            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+            var firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
 
-            int n = Convert.ToInt32(firstMultipleInput[0]);
+            var n = Convert.ToInt32(firstMultipleInput[0]);
 
-            int m = Convert.ToInt32(firstMultipleInput[1]);
+            var m = Convert.ToInt32(firstMultipleInput[1]);
 
-            int result = Result.legoBlocks(n, m);
+            var result = Result.legoBlocks(n, m);
 
             textWriter.WriteLine(result);
         }
