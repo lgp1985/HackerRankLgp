2143447175543648b352ff2036f177543ce16be6
Luis Gustavo Pereira <lgp1985@outlook.com>
2023-06-20 12:03:43 -0300

    fix template from Java 8

--- TreePreorderTraversal/Program.cs
+++ TreePreorderTraversal/Program.cs
@@ -1,8 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-class Solution {
-    static void Main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+﻿class Solution
+{
+    /* you only have to complete the function given below.  
+    Node is defined as
+    */
+
+    public class Node
+    {
+        public Node(int data)
+        {
+            this.data = data;
+        }
+        public int data { get; set; }
+        public Node? left { get; set; }
+        public Node? right { get; set; }
+    }
+
+    public static void preOrder(Node root)
+    {
+        /* Enter your code here. */
+    }
+
+    public static Node insert(Node? root, int data)
+    {
+        if (root == null)
+        {
+            return new Node(data);
+        }
+        else
+        {
+            Node cur;
+            if (data <= root.data)
+            {
+                cur = insert(root.left, data);
+                root.left = cur;
+            }
+            else
+            {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+    static void Main(String[] args)
+    {
+        _ = Console.ReadLine();
+        Node root = null;
+        foreach (var item in Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse))
+        {
+            root = insert(root, item);
+        }
+        preOrder(root);
     }
 }
