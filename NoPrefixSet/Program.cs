f59fc4dae7f1b2c731a5f890a0b5a35eccc7d37a
Luis Gustavo Pereira <lgp1985@outlook.com>
2023-06-20 15:05:58 -0300

    Create NoPrefixSet

---
+++ NoPrefixSet/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'noPrefix' function below.
+     *
+     * The function accepts STRING_ARRAY words as parameter.
+     */
+
+    public static void noPrefix(List<string> words)
+    {
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> words = new List<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string wordsItem = Console.ReadLine();
+            words.Add(wordsItem);
+        }
+
+        Result.noPrefix(words);
+    }
+}
